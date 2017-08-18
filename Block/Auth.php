5e3d4803438c1b78e5e05b3425bcf3c515818ac1
David Umoh <david.umoh@gloo.ng>
2017-08-18 14:22:06 +0100

    Auth0 Login widget has been implemented using auth0-lock.min.js version 10.20.0. Magento advises that module dependencies be injected using requirejs. This did not work for auth0.js or auth0-lock.js. The other option was to include it in the default_header_block - the global header block of Magento. This works without issues. I have opened an issue in the Auth0.js github repo. If i get any headway with that, I'll switch the implementation to inject it via requirejs. Configuration values are loaded from the database. Store through the admin. PHPDoc blocks added for documentation

--- Block/Auth.php
+++ Block/Auth.php
@@ -2,8 +2,41 @@
 namespace DavidUmoh\Auth0\Block;
 
 use Magento\Framework\View\Element\Template;
+use DavidUmoh\Auth0\Helper\Config;
 
 class Auth extends Template{
 
+    /**
+     * @var Config
+     */
+    private $config;
+
+    public function __construct
+    (
+        Template\Context $context,
+        Config $config,
+        array $data = []
+    )
+    {
+        $this->config = $config;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Returns a class of Config options that can be easily json encoded for use in javascript.
+     * Currently has the following options:
+     * - domain - Auth0 domain
+     * - clientId
+     * - callbackUrl
+     *
+     * @return \stdClass
+     */
+    public function getConfigData(){
+        $config = new \stdClass();
+        $config->domain = $this->config->getDomain();
+        $config->clientId = $this->config->getClientId();
+        $config->callbackUrl = $this->config->getCallbackUrl();
+        return $config;
+    }
 
 }
